2c6eb375804cc61a4c58090a3509666784bcd475
MatvienokVS <Skavaava@gmail.com>
2023-01-21 23:17:40 +0300

    Решение ДЗ семинар 5

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -9,51 +9,50 @@ Write("Введите количество элементов массива: ")
 int.TryParse(ReadLine(), out int size_array);
 int[] array = GetArray(size_array);
 PrintArray(array);
-WriteLine(SummOddNum(array));
+SummOddNum(array);
 
 
 int[] GetArray(int size)
 {
-    int[] array = new int[size];
-    Random rnd = new Random();
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = rnd.Next(100);
-    }
-    return array;
+	int[] array = new int[size];
+	Random rnd = new Random();
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = rnd.Next(10);
+	}
+	return array;
 }
 
 int SummOddNum(int[] inArray)
 {
-    int summ = 0;
-    int[] ints = new int[inArray.Length];
+	int summ = 0;
+	int[] ints = new int[inArray.Length];
 
-    foreach (int a in inArray)
-    {
-        if (a % 2 == 0) summ+= inArray[a];
+	for (int a = 0; a < inArray.Length; a++)
+	{
+		if (a % 2 > 0) summ += inArray[a];
+	}
 
-        //summ += a % 2 > 0 ? a : 0;
-    }
-        
-    //for (int i = 0; i < inArray.Length; i++)
-    //{
-    //    if (i % 2 > 0) summ += ints[i];
-    //}
-    
-    return summ;
+
+	//for (int i = 0; i < inArray.Length; i++)
+	//{
+	//    if (i % 2 > 0) summ += ints[i];
+	//}
+	WriteLine(summ);
+	return summ;
 }
 
 
 void PrintArray(int[] array)
 {
-    Write("[");
+	Write("[");
 
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        Write($"{array[i]}, ");
-    }
+	for (int i = 0; i < array.Length - 1; i++)
+	{
+		Write($"{array[i]}, ");
+	}
 
-    Write($"{array[array.Length - 1]}");
-    Write("]");
-    WriteLine();
+	Write($"{array[array.Length - 1]}");
+	Write("]");
+	WriteLine();
 }
--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -6,5 +6,54 @@ using static System.Console;
 Clear();
 
 Write("Введите колличество элементов в массиве: ");
-int.TryParse(ReadLine(), out int size_array);
+int.TryParse(ReadLine()!, out int size_array);
+int[] array = GetArray(size_array);
+PrintArray(array);
+DeffNumMaxMin(array);
 
+
+int[] GetArray(int size)
+{
+	int[] array = new int[size];
+	Random rnd = new Random();
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = rnd.Next(100, 1000);
+	}
+	return array;
+}
+
+void DeffNumMaxMin(int[] inArray)
+{
+	int maxnum = inArray[0];
+	int minnum = inArray[0];
+
+	int[] ints = new int[inArray.Length];
+
+	for (int i = 0; i < inArray.Length; i++)
+	{
+
+		if (maxnum < inArray[i]) maxnum = inArray[i];
+		if (minnum > inArray[i]) minnum = inArray[i];
+	}
+
+	WriteLine(maxnum);
+	WriteLine(minnum);
+	WriteLine(maxnum- minnum);
+    
+}
+
+
+void PrintArray(int[] array)
+{
+	Write("[");
+
+	for (int i = 0; i < array.Length - 1; i++)
+	{
+		Write($"{array[i]}, ");
+	}
+
+	Write($"{array[array.Length - 1]}");
+	Write("]");
+	WriteLine();
+}
